88e67f8cf5a0b28474bed760c664c3c0e5af6b07
pepe6bf <pepe6bf@gmail.com>
2022-07-27 16:41:20 +0900

    Initialize java + gradle

---
+++ fc-java-oop/src/main/java/com/fc/fcjavaoop/FcJavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.fc.fcjavaoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcJavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcJavaOopApplication.class, args);
+	}
+
+}
---
+++ fc-java-oop/src/test/java/com/fc/fcjavaoop/FcJavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.fcjavaoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcJavaOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
