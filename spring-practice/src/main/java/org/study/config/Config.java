9fe4fc28158a8e71185ae4700eab322f4a107f80
pepe6bf <pepe6bf@gmail.com>
2022-12-11 17:18:41 +0900

    P03: Spring web-mvc 적용 및 Tomcat 실행
    web-mvc를 활용해서 REST-API 형태로 리펙토링하고 Tomcat9을 활용해 배포하였다.

--- spring-practice/src/main/java/org/study/config/Config.java
+++ spring-practice/src/main/java/org/study/config/Config.java
@@ -2,7 +2,9 @@ package org.study.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("org.study")
 @Configuration
 public class Config {
